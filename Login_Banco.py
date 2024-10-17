8ccad94d2643fb79b3a0b86c2c9f37eb28896926
Cauã Grigorio <159858060+cauagrigoriodev@users.noreply.github.com>
2024-10-17 16:06:57 -0300

    Login_Banco.py

--- Login_Banco.py
+++ Login_Banco.py
@@ -2,10 +2,11 @@
 # colocando as informações dos clientes
 print("Informe os dados solicitados abaixo")
 clientes = input("Digite seu nome: ")
+#função de validação se o cpf está recendo somente numeros 
 def login():
     while True:
-        cpf = input("Digite seu CPF: ")
-        if cpf.isdigit() and len(cpf) == 11:
+        cpf = input("Digite seu CPF (somente numeros): ")
+        if cpf.isdigit() and len(cpf) == 11: #limte padrão de algoritimos 
             break
         elif cpf.isdigit() and len(cpf) < 11 or cpf.isdigit() and len(cpf) > 11:
             print("Por Favor digite um cpf válido!")
@@ -13,6 +14,7 @@ def login():
             print("Digite somente numeros!")    
 
 login()
+#Função para validar conta bancaria 
 def entrar_contabank():
     while True:
         id_conta = input("Digite o número da sua conta bancária: ")
@@ -23,3 +25,4 @@ def entrar_contabank():
             print("O número da conta bancária está errado. Tente novamente.")
 
 entrar_contabank()
+
